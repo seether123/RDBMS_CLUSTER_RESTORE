a54dfc0285f061aa2e3b469e6c1712aea1c59547
seether123 <46416748+seether123@users.noreply.github.com>
2023-09-29 19:29:30 +0530

    Create restore.py

---
+++ restore.py
@@ -0,0 +1,18 @@
+# main.py
+from rds_snapshot_manager import RDSSnapshotManager
+from config import RDS_REGION, RDS_CLUSTER_IDENTIFIER, NEW_CLUSTER_IDENTIFIER
+
+if __name__ == '__main__':
+    # Create an instance of the RDSSnapshotManager
+    snapshot_manager = RDSSnapshotManager(RDS_REGION, RDS_CLUSTER_IDENTIFIER)
+
+    # List snapshots created today for the specified RDS cluster
+    snapshot_manager.list_snapshots_created_today()
+
+    # Check if a source snapshot was found and print it
+    if snapshot_manager.source_snapshot_identifier:
+        print(f"Source snapshot identifier: {snapshot_manager.source_snapshot_identifier}")
+    else:
+        print("No source snapshot available.")
+    new_cluster_identifier = 'new-cluster-identifier'  # Replace with your desired cluster identifier
+    snapshot_manager.restore_cluster_from_snapshot(NEW_CLUSTER_IDENTIFIER)
