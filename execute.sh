1ec49cfd3b46827151446da97a795964172a9b1e
seether123 <46416748+seether123@users.noreply.github.com>
2023-09-30 17:49:33 +0530

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -7,13 +7,8 @@ python_output=$(python3 restore.py)
 case "$python_output" in
           *"Snapshots created yesterday for cluster"*)
             echo "snaphshot has been created, please wait we are working on restoring and creating a new rds cluster....."
-            python3 rds_instance.py
-            python3 get_cluster_endpoint.py
-            python3 route53_addition.py
-            python3 jira_stop.py
-            python3 jira_start.py
-            python3 url_check.py
-
+            python3 rds_instance.py && python3 get_cluster_endpoint.py && python3 route53_addition.py && \
+            python3 jira_stop.py && python3 jira_start.py && python3 url_check.py
     # Add the command to execute your other script here
            ;;
          *)
