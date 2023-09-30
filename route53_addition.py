2e9322be075122f879010f8be45251ebea25cba5
seether123 <46416748+seether123@users.noreply.github.com>
2023-09-30 15:24:04 +0530

    Update route53_addition.py

--- route53_addition.py
+++ route53_addition.py
@@ -1,5 +1,16 @@
+import boto3
 from endpoint import NEW_CLUSTER_ENDPOINT
+from config import (
+    ROUTE53_HOSTED_ZONE_ID,
+    ROUTE53_RECORD_NAME,
+)
+
+def update_route53_record(cluster_endpoint, hosted_zone_id, record_name):
+    try:
+        # Initialize the Route 53 client
         route53_client = boto3.client('route53')
+
+        # Define the change batch
         change_batch = {
             'Changes': [
                 {
@@ -18,8 +29,16 @@ from endpoint import NEW_CLUSTER_ENDPOINT
             ],
         }
 
-    # Update the Route 53 record set
+        # Update the Route 53 record set
         route53_response = route53_client.change_resource_record_sets(
             HostedZoneId=hosted_zone_id,
             ChangeBatch=change_batch
         )
+
+        print(f"Route 53 record updated successfully!")
+
+    except Exception as e:
+        print(f"Error updating Route 53 record: {str(e)}")
+
+if __name__ == '__main__':
+    update_route53_record(NEW_CLUSTER_ENDPOINT, ROUTE53_HOSTED_ZONE_ID, ROUTE53_RECORD_NAME)
