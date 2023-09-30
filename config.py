74202f53df8909c527b1e045c804e02c658d7f30
seether123 <46416748+seether123@users.noreply.github.com>
2023-09-30 15:22:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,8 @@
 RDS_REGION = 'ap-south-1'
 RDS_CLUSTER_IDENTIFIER = 'database-1'
 NEW_CLUSTER_IDENTIFIER = 'new-cluster-identifier'
+NEW_INSTANCE_IDENTIFIER = 'new-instance'
+RDS_INSTANCE_CLASS = 'db.r5.large'  # Specify your desired instance type here
+RDS_AVAILABILITY_ZONE = 'ap-south-1a'  # Specify your preferred AZ here
+ROUTE53_HOSTED_ZONE_ID = 'your-hosted-zone-id'  # Replace with your hosted zone ID
+ROUTE53_RECORD_NAME = 'your-record-name'  # Replace with your desired record name
